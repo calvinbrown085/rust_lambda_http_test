7591cdd29509f267683b5c72c4be739ff85670f6
Calvin Brown <calvinb085@icloud.com>
2019-10-13 22:45:32 -0500

    Adding kinesis transform

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use lambda_runtime::*;
-use serde::{Serialize, Deserialize};
+use bytes::Bytes;
 use lambda_runtime::error::HandlerError;
+use lambda_runtime::*;
+use rusoto_core::credential::AwsCredentials;
+use rusoto_core::{HttpClient, Region};
+use rusoto_kinesis::{Kinesis, KinesisClient, PutRecordsInput, PutRecordsRequestEntry};
+use rusoto_credential::ProvideAwsCredentials;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Clone)]
 struct KinesisEvent {
     #[serde(rename = "Records")]
-    records: Vec<Record>
+    records: Vec<Record>,
 }
 #[derive(Deserialize, Clone)]
 struct Record {
-    kinesis: KinesisRecord
+    kinesis: KinesisRecord,
 }
 #[derive(Deserialize, Clone)]
 struct KinesisRecord {
@@ -17,16 +22,35 @@ struct KinesisRecord {
     partition_key: String,
     data: String,
 }
-#[derive(Serialize, Deserialize, Clone)]
-struct CustomOutput {
-    message: String
+
+fn main() {
+    lambda!(my_handler)
 }
 
-fn main() {lambda!(my_handler)}
+fn my_handler(e: KinesisEvent, ctx: Context) -> Result<PutRecordsInput, HandlerError> {
+    let kinesis_client = KinesisClient::new(
+        Region::UsEast1,
+    );
 
-fn my_handler(e: KinesisEvent, ctx: Context) -> Result<CustomOutput, HandlerError> {
-    let data_vec: Vec<&String> = e.records.iter().map(|record| &record.kinesis.data).collect();
-    Ok(CustomOutput{
-        message: format!("Kinesis Events: {:#?}", &data_vec),
-    })
+    let data_vec: Vec<&String> = e
+        .records
+        .iter()
+        .map(|record| &record.kinesis.data)
+        .collect();
+    let put_record_vec: Vec<PutRecordsRequestEntry> = e
+        .records
+        .iter()
+        .map(|record| PutRecordsRequestEntry {
+            data: Bytes::from(record.kinesis.data.clone()),
+            explicit_hash_key: None,
+            partition_key: record.kinesis.partition_key.clone(),
+        })
+        .collect();
+    let put_records_input: PutRecordsInput = PutRecordsInput {
+        records: put_record_vec,
+        stream_name: String::from("basic-tweet-stream"),
+    };
+    kinesis_client.put_records(put_records_input.clone());
+
+    Ok(put_records_input)
 }
