39c37d7f1cc2d4f9b6729ca78e8697e604350dde
Calvin Brown <calvinb085@icloud.com>
2019-10-13 21:26:58 -0500

    Adding basic kinesis event

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::*;
+use serde::{Serialize, Deserialize};
+use lambda_runtime::error::HandlerError;
+
+#[derive(Deserialize, Clone)]
+struct KinesisEvent {
+    #[serde(rename = "Records")]
+    records: Vec<Record>
+}
+#[derive(Deserialize, Clone)]
+struct Record {
+    kinesis: KinesisRecord
+}
+#[derive(Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+struct KinesisRecord {
+    partition_key: String,
+    data: String,
+}
+#[derive(Serialize, Deserialize, Clone)]
+struct CustomOutput {
+    message: String
+}
+
+fn main() {lambda!(my_handler)}
+
+fn my_handler(e: KinesisEvent, ctx: Context) -> Result<CustomOutput, HandlerError> {
+    let data_vec: Vec<&String> = e.records.iter().map(|record| &record.kinesis.data).collect();
+    Ok(CustomOutput{
+        message: format!("Kinesis Events: {:#?}", &data_vec),
+    })
 }
